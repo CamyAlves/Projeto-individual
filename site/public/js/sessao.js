7e94d9d0fc944b44b3bb8443f4056d767072d20c
CamillySouza20 <142421290+CamyAlves@users.noreply.github.com>
2023-11-25 22:27:34 -0300

    update tela-jogo

--- site/public/js/sessao.js
+++ site/public/js/sessao.js
@@ -3,6 +3,7 @@ function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
     var pontoTotal = sessionStorage.PONTO_TOTAL;
+    
     var b_usuario = document.getElementById("b_usuario");
     var b_pontuacao = document.getElementById("b_pontuacao");
 
