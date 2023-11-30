4dd0f04b8df72ae4efe10d97ad19d7d9ee54a355
CamillySouza20 <142421290+CamyAlves@users.noreply.github.com>
2023-11-29 22:50:59 -0300

    select dashboard nacional

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -51,9 +51,20 @@ function partida(temaPartida, fkPonto, fkUsuario) {
 }
 
 
+
+function graficoNacional(){
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        select count(idPartida) from partida join usuario on idUsuario = fkUsuario where temaPartida = 'nacional';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
 module.exports = {
     autenticar,
     cadastrar,
     pontuacao,
-    partida
+    partida,
+    graficoNacional
 };
