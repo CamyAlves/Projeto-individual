cdf128fbfbcac4de4dec0b0c0135a4376555fb1c
Camilly Alves <142421290+CamyAlves@users.noreply.github.com>
2023-11-30 15:34:25 -0300

    Update usuarioController.js
    função quantidade partida nacional

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -184,9 +184,28 @@ function partida(req, res) {
             }
         );
 }
+
+function qntPartidaNacional(req, res) {
+
+    var idUsuario = req.params.idUsuario;
+
+    usuarioModel.qntPartidaNacional(idUsuario).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
 module.exports = {
     autenticar,
     cadastrar,
     pontuacao,
-    partida
+    partida,
+    qntPartidaNacional
 }
