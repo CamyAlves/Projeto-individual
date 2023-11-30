5a7d9d21684365948b3099364b6c8981096454dc
Camilly Alves <142421290+CamyAlves@users.noreply.github.com>
2023-11-30 15:32:07 -0300

    Update usuarios.js
    função quantidade partida nacional

--- site/src/routes/usuarios.js
+++ site/src/routes/usuarios.js
@@ -6,7 +6,7 @@ var usuarioController = require("../controllers/usuarioController");
 //Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
 router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
-})
+});
 
 router.post("/autenticar", function (req, res) {
     usuarioController.autenticar(req, res);
@@ -14,11 +14,15 @@ router.post("/autenticar", function (req, res) {
 
 router.post("/pontuacao", function (req, res) {
     usuarioController.pontuacao(req, res);
-})
+});
 
 router.post("/partida", function (req, res) {
     usuarioController.partida(req, res);
-})
+});
+
+router.get("/ultimas/:idUsuario", function (req, res) {
+    usuarioController.qntPartidaNacional(req, res);
+});
 
 
 module.exports = router;
